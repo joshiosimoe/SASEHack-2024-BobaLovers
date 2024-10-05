856ff51971c6a3ab641637591a56084a6abecc5f
Joshua Yustana <joshiosimoe@gmail.com>
2024-10-05 17:02:17 -0500

    Clicking Map Deploys Map Marker

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-
+let map;
+let placeMarker = false;
 
 // Initializing Google Maps API
 (g=>{var h,a,k,p="The Google Maps JavaScript API",c="google",l="importLibrary",q="__ib__",m=document,b=window;b=b[c]||(b[c]={});var d=b.maps||(b.maps={}),r=new Set,e=new URLSearchParams,u=()=>h||(h=new Promise(async(f,n)=>{await (a=m.createElement("script"));e.set("libraries",[...r]+"");for(k in g)e.set(k.replace(/[A-Z]/g,t=>"_"+t[0].toLowerCase()),g[k]);e.set("callback",c+".maps."+q);a.src=`https://maps.${c}apis.com/maps/api/js?`+e;d[q]=f;a.onerror=()=>h=n(Error(p+" could not load."));a.nonce=m.querySelector("script[nonce]")?.nonce||"";m.head.append(a)}));d[l]?console.warn(p+" only loads once. Ignoring:",g):d[l]=(f,...n)=>r.add(f)&&u().then(()=>d[l](f,...n))})({
@@ -8,8 +9,7 @@
     // Add other bootstrap parameters as needed, using camel case.
   });
 
-let map;
-
+  //Create Map
 async function initMap() {
   const { Map } = await google.maps.importLibrary("maps");
 
@@ -17,6 +17,29 @@ async function initMap() {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
   });
+
+  // Create Event on Click
+  google.maps.event.addListener(map, 'click', function (event) {
+    displayCoordinates(event.latLng);
+    
+    new google.maps.Marker({
+        position: { lat: event.latLng.lat(), lng: event.latLng.lng() },
+        map,
+        title: "Hello World!",
+        });
+    });
+
+    function displayCoordinates(pnt) {
+
+    var lat = pnt.lat();
+    lat = lat.toFixed(4);
+    var lng = pnt.lng();
+    lng = lng.toFixed(4);
+    console.log("Latitude: " + lat + "  Longitude: " + lng);
+    }
+
+
+
 }
 
 initMap();
