f85609a719cfebc0b59a9f52f6d976a860832ffa
Joshua Yustana <joshiosimoe@gmail.com>
2024-10-06 21:18:05 -0500

    Working Commit

--- script.js
+++ script.js
@@ -33,6 +33,30 @@ async function initMap() {
 
     map = new Map(document.getElementById("map"), mapOptions);
 
+    // Find User's Location
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition((position) => {
+            const userLocation = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            // Update the map's center and add a marker
+            map.setCenter(userLocation);
+            map.setZoom(15);
+            let newMarker = new google.maps.Marker({
+                position: userLocation,
+                map: map
+            });
+            markers.push(newMarker);
+        }, () => {
+            handleLocationError(true, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, map.getCenter());
+    }
+
     const infoWindow = new google.maps.InfoWindow({
         minWidth: 200,
         maxWidth: 200
@@ -59,7 +83,7 @@ async function initMap() {
             title: "Hello World!",
         });
 
-        currentMarker = marker;
+        let currentMarker = marker;
         console.log("Marker Position:", marker.position.lat + " and " + marker.position.lng);
 
         markers.push(marker);
@@ -78,15 +102,15 @@ async function initMap() {
             <h5>Create Event</h5>
 
             
-            <input id="title-input" type="text" placeholder="Add Title">
+            <input id="title-input" type="text" placeholder="Add Title" autocomplete="off">
 
-            <input id="hostname-input" type="text" placeholder="Host">
+            <input id="hostname-input" type="text" placeholder="Host" autocomplete="off">
 
             <label class="form-label" id="date-input-label">Date:</label>
-            <input id="date-input" type="date">
+            <input id="date-input" type="date" autocomplete="off">
 
             <label class="form-label">Time:</label>
-            <input id="time-input" type="time">
+            <input id="time-input" type="time" autocomplete="off">
 
             <textarea id="description-input" type="text" placeholder="Description"></textarea>
             <button class="btn btn-primary btn-sm" id="submit-event-button" type="submit">Submit</button>
@@ -105,7 +129,7 @@ async function initMap() {
     }
 
     google.maps.event.addListener(infoWindow, 'closeclick', () => {
-        currentMarker.setMap(null);
+        markers[0].setMap(null);
         console.log('InfoWindow closed!');
         // You can perform additional actions here
     });
@@ -130,7 +154,10 @@ function submitEvent() {
     const title = document.getElementById("title-input").value;
     const hostName = document.getElementById("hostname-input").value;
     const date = document.getElementById("date-input").value;
+    const formattedDate = formatDate(date);
+    console.log("formattedDate: ", formattedDate);
     const time = document.getElementById("time-input").value;
+    const formattedTime = formatTime(time);
     const description = document.getElementById("description-input").value;
 
     // const date = new Date(dateInput);
@@ -174,7 +201,7 @@ function submitEvent() {
                 <h5 id="marker-info-title">${title}</h5>
                 <span id="marker-info-host">Host: ${hostName}<br /></span>
                 
-                <p id="marker-info-when">${date} at ${time}</p>
+                <p id="marker-info-when">${formattedDate.month} ${formattedDate.day}, ${formattedDate.year} at ${formattedTime.hour}:${formattedTime.minute} ${formattedTime.dayType}</p>
 
                 <p id="marker-info-description">${description}</p>
             </div>
@@ -185,7 +212,7 @@ function submitEvent() {
     }
     
     console.log("Checkpoint #3", marker);
-    const eventObject = { id: ++id, title: title, hostName: hostName, date: date, time: time, description: description, markerLat: marker.position.lat, markerLng: marker.position.lng };
+    const eventObject = { id: ++id, title: title, hostName: hostName, date: formattedDate, time: formattedTime, description: description, markerLat: marker.position.lat, markerLng: marker.position.lng };
     events.push(eventObject);
     eventObject.marker = marker;
     console.log("Checkpoints #4", eventObject.marker);
@@ -198,12 +225,14 @@ function generateEventsList()
 {
     return events.map(event => 
         `
-        <div key=${event.id}>
-        <p id="event-list-title">${event.title}</p>
-        <p id="event-list-hostName">${event.hostName}</p>
-        <p id="event-list-date">${event.date}</p>
-        <p id="event-list-time">${event.time}</p>
-        <p id="event-list-description">${event.description}</p>
+        <div class="marker-info">
+                <h5 id="marker-info-title">${event.title}</h5>
+                <span id="marker-info-host">Host: ${event.hostName}<br /></span>
+                
+                <p id="marker-info-when">${event.date.month} ${event.date.day}, ${event.date.year} at ${event.time.hour}:${event.time.minute} ${event.time.dayType}</p>
+
+                <p id="marker-info-description">${event.description}</p>
+            </div>
 
         <button type="button" onClick="moveMapToMarkerAtCenter(${event.id}, ${event.markerLat}, ${event.markerLng})">See Location</button>
         </div>
@@ -223,13 +252,15 @@ function moveMapToMarkerAtCenter(id, initLat, initLng) {
         maxWidth: 200
     });
 
+    //${events[id - 1].title}
     const infoWindowContent = `
-            <div class="marker-content">
-                <h1>${events[id - 1].title}</h1>
-                <p>by: ${events[id - 1].hostName}</p>
-                <p>${events[id - 1].date}</p>
-                <p>${events[id - 1].time}</p>
-                <p>${events[id - 1].description}</p>
+            <div class="marker-info">
+                <h5 id="marker-info-title">${events[id - 1].title}</h5>
+                <span id="marker-info-host">Host: ${events[id - 1].hostName}<br /></span>
+                
+                <p id="marker-info-when">${events[id - 1].date.month} ${events[id - 1].date.day}, ${events[id - 1].date.year} at ${events[id - 1].time.hour}:${events[id - 1].time.minute} ${events[id - 1].time.dayType}</p>
+
+                <p id="marker-info-description">${events[id - 1].description}</p>
             </div>
         `;
 
@@ -270,7 +301,88 @@ hamburgerMenu.addEventListener('click', function() {
         isSidebarOpen = false; // Update the state
 }});
 
-function returnDate(date) {
-    year = date.splice(0,4);
-    month = date.splice
+function formatDate(date) {
+    let dateObj = {};
+    year = date.slice(0,4);
+    month = convertMonth(date.slice(5,7));
+    day = date.slice(8, 10);
+    dateObj.year = year;
+    dateObj.month = month;
+    dateObj.day = day;
+    return dateObj;
+}
+
+function convertMonth(monthNum)
+{
+    let month;
+    switch(monthNum)
+    {
+        case "01":
+            month = "January";
+            break;
+        case "02":
+            month = "February";
+            break;   
+        case "03":
+            month = "March";
+            break;
+        case "04":
+            month = "April";
+            break;
+        case "05":
+            month = "May";
+            break;
+        case "06":
+            month = "June";
+            break;
+        case "07":
+            month = "July";
+            break;
+        case "08":
+            month = "August";
+            break;
+        case "09":
+            month = "September";
+            break;
+        case "10":
+            month = "October";
+            break;
+        case "11":
+            month = "November";
+            break;
+        case "12":
+            month = "December";
+            break;
+        default:
+            console.log("Default");
+    }
+    return month;
+
+}
+
+function formatTime(time) {
+    let formattedTime = {};
+    let hour = parseInt(time.slice(0,2));
+    let minute = time.slice(3, 5);
+    let dayType = "AM";
+
+    if(hour > 12)
+    {
+        hour -= 12;
+        dayType = "PM";
+        hour = hour.toString();
+    }
+
+    formattedTime.hour = hour;
+    formattedTime.minute = minute;
+    formattedTime.dayType = dayType;
+
+    console.log(formattedTime);
+    return formattedTime;
+}
+
+function handleLocationError(browserHasGeolocation, pos) {
+    alert(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
 }
